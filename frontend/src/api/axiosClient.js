cf9048270c4439a869ed2e1329e3b9ab77729d39
Premsaiboga <bogapremsai26@gmail.com>
2025-11-26 16:29:45 +0530

    bakend

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://hrms-xrqy.onrender.com/api/"
+  
+  
+  // http://127.0.0.1:8000/api/,
 });
 
 
