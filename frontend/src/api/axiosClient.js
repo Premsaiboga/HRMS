f013f6445e758bf465d2ee1dfe392dfae6188529
Premsaiboga <bogapremsai26@gmail.com>
2025-11-26 15:17:28 +0530

    updated axios url

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://hrms-xrqy.onrender.com",
 });
 
 // ✅ Automatically attach JWT token for every request
